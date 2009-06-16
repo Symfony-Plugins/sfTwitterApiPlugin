8421f2c09572e43815aec96b0a246c1057676ac8
hugo.hamon <hugo.hamon@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-16 17:51:15 -0500

    [sfTwitterApiPlugin] added new accessors and mutators methods
    git-svn-id: http://svn.symfony-project.com/plugins/sfTwitterApiPlugin/trunk@19327 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/adapter/sfTwitterHttpAdapter.class.php
+++ lib/adapter/sfTwitterHttpAdapter.class.php
@@ -4,11 +4,79 @@ abstract class sfTwitterHttpAdapter
 {
 	protected $username = '';
   protected $password = '';
+  protected $uri = '';
+  protected $method = '';
+  protected $parameters = array();
   protected $statusCode = null;
   protected $connection = null;
-	
-	abstract public function send($uri, $method = 'get', $params = array());
-	
+
+  /**
+   * Sends the request
+   *
+   * @return string The response
+   */
+	abstract public function send();
+
+  /**
+   * Sets the uri
+   *
+   * @param string $uri The uri
+   */
+  public function setUri($uri)
+  {
+    $this->uri = $uri;
+  }
+
+  /**
+   * Returns the uri
+   *
+   * @return string
+   */
+  public function getUri()
+  {
+    return $this->uri;
+  }
+
+  /**
+   * Sets the http method
+   *
+   * @param string $method
+   */
+  public function setMethod($method)
+  {
+    $this->method = $method;
+  }
+
+  /**
+   * Returns the http method
+   *
+   * @return string
+   */
+  public function getMethod()
+  {
+    return $this->method;
+  }
+
+  /**
+   * Sets the request parameters
+   *
+   * @param array $parameters An associative array of key => value pair
+   */
+  public function setParameters(array $parameters)
+  {
+    $this->parameters = $parameters;
+  }
+
+  /**
+   * Returns the request parameters
+   *
+   * @param array The request parameters
+   */
+  public function getParameters()
+  {
+    return $this->parameters;
+  }
+
   /**
    * Sets the status code
    *
